29d86b55c296011d3586c61f756c387583a98b1f
Juan Biscaia <juanbiscaia@gmail.com>
2018-05-07 22:40:48 -0300

    WIP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import ConversationPanel from './components/ConversationPanel';
+import ConversationPanel from './ConversationPanel/index';
 
 class App extends Component {
   render() {
