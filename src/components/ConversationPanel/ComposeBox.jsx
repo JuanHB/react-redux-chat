e72eb48a1bda7d1e57e27c9e8eefbe977cf53915
Juan Biscaia <juanbiscaia@gmail.com>
2018-05-08 22:03:22 -0300

    wip

--- src/components/ConversationPanel/ComposeBox.jsx
+++ src/components/ConversationPanel/ComposeBox.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+
+import Paper from 'material-ui/Paper';
+import IconButton from 'material-ui/IconButton';
+import Send from 'material-ui/svg-icons/content/send';
+
 const ComposeBox = () => {
   return (
-    <div> Compose Box </div>
+    <Paper style={styles.paper}>
+      <input type="text" style={styles.inputText} />
+      <IconButton>
+        <Send/>
+      </IconButton>
+    </Paper>
   );
+};
+
+const styles = {
+  paper: {
+    position: "fixed",
+    zIndex: 100,
+    bottom: 0,
+    left: 0,
+    width: "100%",
+    //padding: 8,
+    height: 50
+  },
+  inputText: {
+    width: "80%",
+    //height: 30,
+    fontSize: 15,
+    //padding: 2,
+    border: "white"
+  }
 };
 
 export default ComposeBox;
